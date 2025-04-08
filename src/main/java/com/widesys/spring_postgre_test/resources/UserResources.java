46ebf4021e36fb359fc74e17c1d04639dffe5489
Glauce Alves <glaucemail@gmail.com>
2025-04-08 10:47:50 -0300

    Service layer, component registration

--- src/main/java/com/widesys/spring_postgre_test/resources/UserResources.java
+++ src/main/java/com/widesys/spring_postgre_test/resources/UserResources.java
@@ -1,16 +1,23 @@
 package com.widesys.spring_postgre_test.resources; 
  
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.widesys.spring_postgre_test.entities.User;
+import com.widesys.spring_postgre_test.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResources {
+	
+	@Autowired
+	private UserService userService;
+	
 	
 	@GetMapping
 	public ResponseEntity<User> findAll(){
@@ -18,6 +25,11 @@ public class UserResources {
 		return ResponseEntity.ok().body(u);
 	}
 
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+	User objUser = userService.findById(id);
+	return ResponseEntity.ok().body(objUser);
+	}
 	
 	
 }
---
+++ src/main/java/com/widesys/spring_postgre_test/services/UserService.java
@@ -0,0 +1,27 @@
+package com.widesys.spring_postgre_test.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.widesys.spring_postgre_test.entities.User;
+import com.widesys.spring_postgre_test.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> objUser =  userRepository.findById(id);
+		return objUser.get();
+	}
+	
+}
