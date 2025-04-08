2b87e732533a8e6a7c21be0cb2d5a68a28e7021a
Glauce Alves <glaucemail@gmail.com>
2025-04-08 10:08:52 -0300

    Project Created

---
+++ src/main/java/com/widesys/spring_postgre_test/SpringPostgreTestApplication.java
@@ -0,0 +1,13 @@
+package com.widesys.spring_postgre_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostgreTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPostgreTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/widesys/spring_postgre_test/SpringPostgreTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.widesys.spring_postgre_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostgreTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
