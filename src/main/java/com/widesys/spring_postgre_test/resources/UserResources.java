902e8d78e777e6330edc2e2dacdc8ef215398836
Glauce Alves <glaucemail@gmail.com>
2025-04-08 10:29:06 -0300

    User entity and resource

---
+++ src/main/java/com/widesys/spring_postgre_test/resources/UserResources.java
@@ -0,0 +1,23 @@
+package com.widesys.spring_postgre_test.resources; 
+ 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.widesys.spring_postgre_test.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResources {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+
+	
+	
+}
